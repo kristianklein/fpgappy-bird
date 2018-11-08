fff9fd0f6dd83f1783ed627b686343a8b0065f7d
Kristian Klein <kristian.klein90@gmail.com>
2018-11-08 10:15:32 +0100

    Add 12.5 MHz clock scaler for ADC. Syntax checked.

--- clockscaler_25M.vhd
+++ clockscaler_25M.vhd
@@ -2,7 +2,7 @@
 -- 50 MHz clock must be scaled down by 2
 -- to get a 25 MHz clock
 -- This means the output signal must be toggled
--- every other rising edge of the 50 MHz clock
+-- every rising edge of the 50 MHz clock
 -- 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -15,22 +15,17 @@ entity clockscaler_25M is
 end clockscaler_25M;
 
 architecture Behavioral of clockscaler_25M is
-	SIGNAL counter : STD_LOGIC_VECTOR (15 DOWNTO 0) := (OTHERS => '0');
 	SIGNAL CLK_25M_sig : STD_LOGIC := '0';
 begin
 	CLK_25M <= CLK_25M_sig; -- Map intermediate signal to output
 	
 	-- Count number of rising edges on 50 MHz clock
 	-- and invert the intermediate signal every
-	-- other rising edge
+	-- rising edge
 	PROCESS (CLK)
 	BEGIN
 		IF rising_edge(CLK) THEN
-			IF (counter >= 1) THEN
-				CLK_25M_sig <= NOT CLK_25M_sig; -- Invert intermediate signal
-			ELSE
-				counter <= counter + 1; -- Note: Signal is not updated until end of process!
-			END IF;
+			CLK_25M_sig <= NOT CLK_25M_sig;
 		END IF;
 	END PROCESS;
 end Behavioral;
